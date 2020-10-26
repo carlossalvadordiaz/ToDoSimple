0ee4e1a54cf6f5dbb64a2bd7b84e5fbddf0323d6
carlossalvadordiaz <csd95@hotmail.com>
2020-10-26 08:43:57 +0100

    Capturados objetos para trabajar con el DOM

---
+++ js/eventos.js
@@ -0,0 +1,13 @@
+//Capturar objetos del DOM
+
+let inputAddTareas = document.getElementById('inputAddTarea');
+
+let selectPrioridadGuardar = document.getElementById('selectPrioridadGuardar')
+
+let btnGuardar = document.querySelector('#btnGuardar');
+
+let selectPrioridadBuscar = document.getElementById('selectPrioridadBuscar');
+
+let buscadorTarea = document.getElementById('buscadorTarea');
+
+let seccionTareas = document.querySelector('#seccionTareas');
