09d5fd2210c910297ff05ee7b5848a66c99f03fc
carlossalvadordiaz <csd95@hotmail.com>
2020-10-26 08:42:34 +0100

    maquetacion basica y creación de datos

---
+++ js/datos.js
@@ -0,0 +1,19 @@
+let listaTareas = new Array();
+
+listaTareas = [
+    {
+        idTarea: 0,
+        titulo: 'Estudiar Javascript',
+        prioridad: 'urgente'
+    },
+    {
+        idTarea: 2,
+        titulo: 'Dormir',
+        prioridad: 'diaria'
+    },
+    {
+        idTarea: 3,
+        titulo: 'Hacer la comida',
+        prioridad: 'mensual'
+    }
+]
